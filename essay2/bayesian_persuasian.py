b88d555f844692f334c5710f23fce35e70ae8ad9
peitalin <n6378056@gmail.com>
2016-01-30 11:52:26 +1000

    crawford sobel

--- essay2/bayesian_persuasian.py
+++ essay2/bayesian_persuasian.py
@@ -13,44 +13,40 @@ Alpha = [float(x) for x in linspace(0, 1, 1000)]
 alpha = round(random.uniform(0.1,1), 1)
 w = round(random.uniform(0.1,1), 1)
 
-wstar = w * 1.5
-astar = alpha * w + (1-alpha) * wstar
 
 
-def a3(mu, k):
-    if mu < k/4:
-        return 0
-    elif k/4 <= mu < k/2:
-        return (1+mu)*k/2
-    else:
-        return (1+mu)*mu
+
+
+"""
+Tenure EXAMPLE
+"""
 
 def v(a, w, k):
     if a*w > k:
-        return a*w - a**2
+        return a*w - (a-w)**2
     else:
-        return -(a**2)
+        return -(a-w)**2
 
 
-def a(mu):
-    w = (1+mu)
-    if v
+plot(A, [v(a, w=1, k=k) for a in A], linestyle='-', label="w={}".format(1))
+
+optimal_a = []
+for ww in linspace(1, 2, 20):
+    yy = [v(a, w=ww, k=k) for a in A]
+    opt_a = yy.index(max(yy))/1000, max(yy)
+    # print("a* = ({}, {})".format(*eq))
+    plot(A, yy, linestyle='-.' )
+    optimal_a.append(opt_a)
 
 
-plot(A, [v(a, w=1, k=1.2) for a in A], linestyle='--')
-plot(A, [v(a, w=2, k=1.2) for a in A], linestyle='-')
+plot(A, [v(a, w=1.42, k=k) for a in A], linestyle='-', label="w={}".format(1.4))
+plot(A, [v(a, w=2, k=k) for a in A], linestyle='-', label='w={}'.format(2))
+legend(loc='lower right')
 
-for ww in linspace(1.1, 1.9, 9):
-    plot(A, [v(a, w=ww, k=1.2) for a in A], linestyle=':')
+plot(*list(zip(*optimal_a)), label='Optimal a*', linestyle='--', color='black')
 
+###############################################################################
 
-
-
-"""
-We are interesting in the way the equilibrium depend on the extent of preference disagreement:
-    captured by alpha and w_star differences.
-Increase in alpha or decrease in w_star makes preferences more aligned according to the definition in Section IV
-"""
 
 
 
@@ -81,12 +77,27 @@ def u2(a, w, k=0.5, l=2.25):
 
 # utility function must be function of actions and state ONLY
 plot(A, [u2(a, w=1, k=k, l=l) for a in A], linestyle='-', label='w=1')
-plot(A, [u2(a, w=2, k=k, l=l) for a in A], linestyle='--', label='w=2')
 for ww in linspace(1.2, 1.8, 4):
     plot(A, [u2(a, w=ww, k=k, l=l) for a in A], linestyle='-.', label='w={}'.format(ww))
+plot(A, [u2(a, w=2, k=k, l=l) for a in A], linestyle='-', label='w=2')
 legend()
 # for low levels of k, both utility functions have a positive point
 # for high levels of k, only w=2 has a position point.
+
+plot(A, [u2(a, w=1, k=k, l=l) for a in A], linestyle='-', label='w=1')
+optimal_a = []
+for ww in linspace(1, 2, 20):
+    yy = [u2(a, w=ww, k=k, l=l) for a in A]
+    opt_a = yy.index(max(yy))/1000, max(yy)
+    # print("a* = ({}, {})".format(*eq))
+    plot(A, yy, linestyle='-.')
+    optimal_a.append(opt_a)
+
+plot(A, [u2(a, w=2, k=k, l=l) for a in A], linestyle='-', label='w=2')
+plot(*list(zip(*equilibria)), linestyle='--', color='black', label='Optimal a*')
+legend(loc='lower right')
+
+
 
 
 def action(a, mu, k):
@@ -126,26 +137,26 @@ def u(a, mu, k=1.5):
     else:
         return effort
 
-k=1.1 # some reference level, anchor
-plot(A, [u(a, mu=0.2, k=k) for a in A], linestyle='-')
-plot(A, [u(a, mu=0.4, k=k) for a in A], linestyle='--')
-plot(A, [u(a, mu=0.8, k=k) for a in A], linestyle='-.')
+# k=1.1 # some reference level, anchor
+# plot(A, [u(a, mu=0.2, k=k) for a in A], linestyle='-')
+# plot(A, [u(a, mu=0.4, k=k) for a in A], linestyle='--')
+# plot(A, [u(a, mu=0.8, k=k) for a in A], linestyle='-.')
 
 
 
 
-# prospect theory kinked utility
-MU = linspace(-1, 1, 2000)
-k=0.5
-# k=0.3
-plot(MU, [v5(mu, k, l=1) for mu in MU], linestyle='-')
-plot(MU, [v5(mu, k, l=2.25) for mu in MU], linestyle='--')
-plot(MU, [v5(mu, k, l=5) for mu in MU], linestyle='--')
+# # prospect theory kinked utility
+# MU = linspace(-1, 1, 2000)
+# k=0.5
+# # k=0.3
+# plot(MU, [v5(mu, k, l=1) for mu in MU], linestyle='-')
+# plot(MU, [v5(mu, k, l=2.25) for mu in MU], linestyle='--')
+# plot(MU, [v5(mu, k, l=5) for mu in MU], linestyle='--')
 
 
 
-plot(MU, [v3(mu, k=1) for mu in MU], linestyle='-')
-plot(MU, [v4(mu, k=1) for mu in MU], linestyle='-')
+# plot(MU, [v3(mu, k=1) for mu in MU], linestyle='-')
+# plot(MU, [v4(mu, k=1) for mu in MU], linestyle='-')
 
 
 
